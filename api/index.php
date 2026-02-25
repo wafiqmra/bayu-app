ff39550ba2afc88b6422cc2dc73b64a0f9328b48
wafiqmra <wafiq.mariatul@mhs.itenas.ac.id>
2026-02-25 22:15:38 +0700

    Fix: Remove hardcoded bootstrap cache files

--- api/index.php
+++ api/index.php
@@ -1,18 +1,10 @@
 <?php
-
-// 1. Jalankan Autoload
 require __DIR__ . '/../vendor/autoload.php';
-
-// 2. Bootstrapping Laravel
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// 3. Trik Sakti: Paksa jalur storage ke /tmp agar tidak Error 500
-$app->useStoragePath('/tmp/storage');
-if (!is_dir('/tmp/storage/framework/views')) {
-    mkdir('/tmp/storage/framework/views', 0755, true);
-}
+// Paksa folder storage ke /tmp
+$app->useStoragePath('/tmp');
 
-// 4. Jalankan Kernel
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
