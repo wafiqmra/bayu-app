47c390bd701ff8587103214afa6a96b32c755034
wafiqmra <wafiq.mariatul@mhs.itenas.ac.id>
2026-02-25 22:49:29 +0700

    Fix: Force storage path and view compiled path

--- api/index.php
+++ api/index.php
@@ -2,6 +2,13 @@
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
+/*
+|--------------------------------------------------------------------------
+| Trik Khusus Vercel: Pindahkan Storage ke /tmp
+|--------------------------------------------------------------------------
+*/
+$app->useStoragePath('/tmp');
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
