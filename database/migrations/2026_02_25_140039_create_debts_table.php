e642bbaa1f00e05a8c0d2817267d3a49f23a1296
wafiqmra <wafiq.mariatul@mhs.itenas.ac.id>
2026-02-25 21:08:30 +0700

    Initial Commit: Laravel terhubung ke Supabase dan Tabel Utang siap

---
+++ database/migrations/2026_02_25_140039_create_debts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('debts', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_peminjam');
+            $table->integer('jumlah_utang');
+            $table->string('keterangan')->nullable();
+            $table->enum('status', ['belum_lunas', 'lunas'])->default('belum_lunas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('debts');
+    }
+};
