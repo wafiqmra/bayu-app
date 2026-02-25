1e3b0e46f7f1028781037163dac5a51659f155b0
wafiqmra <wafiq.mariatul@mhs.itenas.ac.id>
2026-02-25 22:09:56 +0700

    Fix: Force storage path to tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -1,17 +1,21 @@
 <?php
 
-// Jalankan autoload manual agar vendor terbaca
+// 1. Jalankan Autoload
 require __DIR__ . '/../vendor/autoload.php';
 
-// Panggil aplikasi Laravel
+// 2. Bootstrapping Laravel
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+// 3. Trik Sakti: Paksa jalur storage ke /tmp agar tidak Error 500
+$app->useStoragePath('/tmp/storage');
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
 
+// 4. Jalankan Kernel
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
-
 $response->send();
-
 $kernel->terminate($request, $response);
