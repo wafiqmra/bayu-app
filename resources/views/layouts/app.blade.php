ec5e43fbec7367740855962332772d9998b2ff45
wafiqmra <wafiq.mariatul@mhs.itenas.ac.id>
2026-02-28 13:19:13 +0700

    Fix: Dropdown bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         <script src="https://cdn.tailwindcss.com"></script>
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     </head>
     <body class="font-sans antialiased">
